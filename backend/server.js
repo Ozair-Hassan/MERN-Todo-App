42197e967c601340d0b7a1b64252275df99b9695
Ozair-Hassan <ozair.hassan123@gmail.com>
2024-08-04 22:55:57 +0500

    fix: production deployment build command

--- backend/server.js
+++ backend/server.js
@@ -6,30 +6,42 @@ import connectDB from './config/db.js'
 import authRoutes from './routes/authRoutes.js'
 import itemRoutes from './routes/itemRoutes.js'
 
+// Load environment variables based on the environment
 if (process.env.NODE_ENV === 'test') {
   dotenv.config({ path: '.env.test' })
 } else {
   dotenv.config()
 }
+
 const port = process.env.PORT || 5000
 connectDB()
 
 const app = express()
 app.use(express.json())
 
+// API routes
 app.use('/api/auth', authRoutes)
 app.use('/api/item', itemRoutes)
+
+// Log the current environment
+console.log(`Running in ${process.env.NODE_ENV} mode`)
+
 if (process.env.NODE_ENV === 'production') {
   const __filename = fileURLToPath(import.meta.url)
   const __dirname = path.dirname(__filename)
 
-  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+  // Log the paths for debugging
+  const staticFilesPath = path.join(__dirname, '../frontend/dist')
+  const indexPath = path.resolve(__dirname, '../frontend/dist', 'index.html')
 
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html')
-    )
-  )
+  console.log('Serving static files from:', staticFilesPath)
+  console.log('Index file path:', indexPath)
+
+  // Serve the static files from the dist directory
+  app.use(express.static(staticFilesPath))
+
+  // Handle any other routes to serve the index.html file
+  app.get('*', (req, res) => res.sendFile(indexPath))
 } else {
   app.get('/', (req, res) => res.send('Please set to production'))
 }
